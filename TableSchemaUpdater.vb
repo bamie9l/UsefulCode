583e0b3bf09b53a5e32e80b6aa33a5a09c203f0e
bamie9l <bamie9l@gmail.com>
2013-01-22 22:33:49 +0000

    Update TableSchemaUpdater.vb
    Avoids system tables, added some extra lines that are useful for altering tables en masse

--- TableSchemaUpdater.vb
+++ TableSchemaUpdater.vb
@@ -6,17 +6,21 @@ Option Explicit
 ''' </summary>
 Private Sub AlterTableSchema()
 
+    Dim fld As Field
     Dim dbs As Database, tdf As TableDef
     Set dbs = CurrentDb
     
     For Each tdf In dbs.TableDefs
-        For Each fld In tdf.Fields
-            If fld.Name = "LastAmendedWhen" Then
-                'DoCmd.RunSQL ("ALTER TABLE " & tdf.Name & " DROP Column " & fld.Name & ";")
-                Debug.Print "Table Name: " & tdf.Name & ", Fieldname was: " & fld.Name
-                fld.Name = "LastAmendedDate"
-            End If
-        Next
+        If Left(tdf.Name, 4) <> "MSys" Then
+            'For Each fld In tdf.Fields
+                'If fld.Name = "Archived" Or fld.Name = "Archive" Then
+                    'DoCmd.RunSQL ("ALTER TABLE " & tdf.Name & " DROP Column " & fld.Name & ";")
+                    DoCmd.RunSQL ("ALTER TABLE " & tdf.Name & " ADD COLUMN LastAmendedByUserName TEXT(255);")
+                    'Debug.Print "Table Name: " & tdf.Name & ", Fieldname was: " & fld.Name
+'                    fld.Name = "Id"
+                'End If
+            'Next
+        End If
     Next
     
     dbs.Close
